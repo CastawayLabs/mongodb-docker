4c27bd98fcb4033ecc2935336a1ecf4690d71055
Matej Kramny <git@matej.me>
2014-08-11 21:29:22 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "Replica Set => $REPL_SET"
-mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET"
+if [ ! -z "$REPL_SET" ]
+then
+  echo "Replica Set => $REPL_SET"
+  echo "replSet=$REPL_SET" >> /etc/mongod.conf
+fi
+
+if [ "$1" -eq "setup" ]
+then
+  echo "Performing Initial Setup"
+  echo "db.createUser({ user: '$2', pwd: '$2', roles: [{ role: 'userAdminAnyDatabase', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }] })" > /tmp/setup.js
+  
+  mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET" --noauth 2>&1 > /dev/null &
+  mongod_pid=$!
+  
+  wait $mongod_pid
+  
+  mongo admin /tmp/setup.js
+else
+  mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET" --auth
+fi
