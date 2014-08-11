37200bd08ec010a0034802a1e1cc8c93cf8d31e1
Matej Kramny <git@matej.me>
2014-08-11 21:32:12 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# Add replica set to mongodb config
 if [ ! -z "$REPL_SET" ]
 then
   echo "Replica Set => $REPL_SET"
   echo "replSet=$REPL_SET" >> /etc/mongod.conf
 fi
 
+# Rsyslog setup
+if [ ! -z "$RSYSLOG" ]
+then
+  echo "Adding RSYSLOGD configuration"
+  
+  echo "$RSYSLOG" >> /etc/rsyslog.conf
+fi
+
+# Setup mongodb user
+# $1 'setup' to do setup
+# $2 user mongodb username
+# $3 user mongodb password
+
 if [ "$1" -eq "setup" ]
 then
   echo "Performing Initial Setup"
-  echo "db.createUser({ user: '$2', pwd: '$2', roles: [{ role: 'userAdminAnyDatabase', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }] })" > /tmp/setup.js
+  echo "db.createUser({ user: '$2', pwd: '$3', roles: [{ role: 'userAdminAnyDatabase', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }] })" > /tmp/setup.js
   
   mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET" --noauth 2>&1 > /dev/null &
   mongod_pid=$!
@@ -17,6 +31,9 @@ then
   wait $mongod_pid
   
   mongo admin /tmp/setup.js
-else
-  mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET" --auth
+  
+  kill $mongod_pid
+  exit 0
 fi
+
+mongod --config /etc/mongod.conf --smallfiles --replSet "$REPL_SET" --auth
